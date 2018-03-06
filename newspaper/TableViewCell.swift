8fd49d2f7f913ea314bcc5210a5ed3acb7184939
serhatburak.yildirim <serhatburak.yildirim@n11.com>
2018-03-06 23:37:23 +0300

    first commit

---
+++ newspaper/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  newspaper
+//
+//  Created by EVREN BAYRAK on 15/12/2016.
+//  Copyright © 2016 EVREN BAYRAK. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ newspaper/viewController2.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController2.swift
+//  newspaper
+//
+//  Created by EVREN BAYRAK on 15/12/2016.
+//  Copyright © 2016 EVREN BAYRAK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
